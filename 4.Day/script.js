8b5f9de391be5bda2630bb0758a6606899b80d25
marcio lira <machollanda@gmail.com>
2021-07-27 17:20:11 -0300

    updates in functions

--- 4.Day/script.js
+++ 4.Day/script.js
@@ -1,6 +1,6 @@
 // initial data
 let square = {
-    a1: '', a2: '',a3: '',
+    a1: '', a2: '', a3: '',
     b1: '', b2: '', b3: '',
     c1: '', c2: '', c3: ''
 
@@ -13,23 +13,29 @@ let playing = false;
 reset();
 // Events
 document.querySelector('.reset').addEventListener('click', reset);
-document.querySelector('.item').array.forEach(item => {
+document.querySelectorAll('.item').forEach(item => {
     item.addEventListener('click', itemClick)
 });
 
 
 // Functions
 function itemClick(event){
-    
+    let item = event.target.getAttribute('data-item');
+    // console.log("clicou em", item)
+    if(square[item] === ''){
+        square[item] = player;
+        renderSquare();
+        
+    }
 }
 
 function reset(){
-    warning= '';
+    warning = '';
 
     let random = Math.floor(Math.random() * 2);
     player = (random === 0) ? 'x' : 'o';
 
-    for(leti in square){
+    for(let i in square){
         square[i] = '';
     }
     playing = true;
@@ -43,11 +49,29 @@ function renderSquare(){
         let item = document.querySelector(`div[data-item=${i}]`);
         item.innerHTML = square[i];
     }
+    checkGame();
 }
 function renderInfor(){
     document.querySelector('.vez').innerHTML = player;
     document.querySelector('.resultado').innerHTML = warning;
 }
 
+function togglePlayer(){
+    player = (player === 'x') ? 'o' : 'x';
+    renderInfor();
+}
 
+function checkGame(){
+    if(checkWinnerFor('x')){
+        warning = 'O "x" venceu';
+        playing = false;
+    }else if(checkWinnerFor('o')){
+        warning = 'O "o" venceu';
+        playing = false;
+    }else if(isFull()){
+        warning = 'Deu empate';
+        playing = false;
+    }
+
+}
 

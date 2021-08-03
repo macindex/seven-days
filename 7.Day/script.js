66fe45cc3188306b1c02dc6834340ced1e758c1a
marcio lira <machollanda@gmail.com>
2021-08-02 23:39:45 -0300

    update seventh day

---
+++ 7.Day/script.js
@@ -0,0 +1,89 @@
+let areas = {
+    a: null,
+    b: null,
+    c: null
+};
+
+document.querySelectorAll(".item").forEach((item) => {
+  item.addEventListener("dragstart", dragstart);
+  item.addEventListener("dragend", dragend);
+});
+
+document.querySelectorAll(".area").forEach((area) => {
+  area.addEventListener("dragover", dragOver);
+  area.addEventListener("dragleave", dragLeave);
+  area.addEventListener("drop", drop);
+});
+
+document.querySelector('.neutralArea').addEventListener('dragover', dragOverNeutral);
+document.querySelector('.neutralArea').addEventListener('dragleave', dragLeaveNeutral);
+document.querySelector('.neutralArea').addEventListener('drop', dropNeutral);
+
+function dragstart(e) {
+  e.currentTarget.classList.add("dragging");
+}
+
+function dragend(e) {
+  e.currentTarget.classList.end("dragging");
+}
+
+function dragOver(e) {
+  if (e.currentTarget.querySelector(".item") === null) {
+    e.preventDefault();
+    e.currentTarget.classList.add("hover");
+  }
+}
+
+function dragLeave(e) {
+  e.currentTarget.classList.remove("hover");
+}
+
+function drop(e) {
+  e.currentTarget.classList.remove("hover");
+
+  let dragItem = document.querySelector(".item.dragging");
+
+  if (e.currentTarget.querySelector(".item") === null) {
+    e.currentTarget.appendChild(dragItem);
+    updateAreas();
+  }
+}
+
+// function neutralArea
+
+function dragOverNeutral(e){
+    e.preventDefault();
+    e.currentTarget.classList.add('hover');
+}
+
+function dragLeaveNeutral(e){
+    e.currentTarget.classList.remove('hover')
+}
+
+function dropNeutral(e){
+    e.currentTarget.classList.remove('hover');
+
+    let dragItem = document.querySelector(".item.dragging");
+    e.currentTarget.appendChild(dragItem);
+    updateAreas();
+}
+
+// Logic functions
+
+function updateAreas(){
+    document.querySelectorAll('.area').forEach(area => {
+        let name = area.getAttribute('data-name');
+
+        if(area.querySelector('.item')!== null) {
+            areas[name] = area.querySelector('.item').innerHTML;
+        }else{
+            areas[name] = null;
+        }
+    });
+    if(areas.a === '1' && areas.b === '2' && areas.c === '3'){
+        document.querySelector('.areas').classList.add('correct');
+    }else{
+        document.querySelector('.areas').classList.remove('correct');
+
+    }
+}
